af372be5a4732574418e600ee780fa807bf52658
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 23:27:50 +0000

    QUERIA COMO ESTAVA COM OS TRAÇOS NO TAMANHO MAIOR POREM COM MAIS QUANTID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                     style={{
                       backgroundImage:
                         'linear-gradient(to bottom, hsl(var(--foreground)) 50%, transparent 50%)',
-                      backgroundSize: '1px 12px',
+                      backgroundSize: '1px 20px',
                       backgroundRepeat: 'repeat-y',
                     }}
                   ></div>
