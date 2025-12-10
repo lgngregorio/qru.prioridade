76d4273c34ce1d40fa786707066fe17d6848a8fe
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 19:32:29 +0000

    deixe as duas mais perto uma da outra  (_for element div_)

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -32,7 +32,7 @@ export default function AppSidebar() {
         <div className="flex items-center justify-center space-x-2">
            <h1 className="text-2xl font-bold text-white flex items-center justify-center gap-2">
             QRU
-            <div className="flex h-6 items-center gap-1.5">
+            <div className="flex h-6 items-center gap-1">
               <div className="w-0.5 h-full bg-white"></div>
               <div className="w-0.5 h-full bg-repeat-y bg-[length:3px_18px] bg-center animate-move-dashes" style={{ backgroundImage: "linear-gradient(to bottom, white 50%, transparent 50%)"}}></div>
             </div>
