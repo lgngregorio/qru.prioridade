52a44adc4232253461810bcfdb063bf4fba35829
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 13:02:49 +0000

    O TAMANHO PRECISA SER IGUAL O DA ABA TO01 (_for element div_)

--- src/components/forms/IncendioForm.tsx
+++ src/components/forms/IncendioForm.tsx
@@ -275,7 +275,7 @@ export default function IncendioForm({ categorySlug }: { categorySlug: string })
           </div>
         </div>
 
-        <div className="flex flex-col sm:flex-row gap-4 pt-6">
+        <div className="flex sm:flex-row gap-4 pt-6">
           <Button size="lg" className="flex-1 bg-green-600 hover:bg-green-700 uppercase text-base" onClick={handleShare}>
               <Share className="mr-2 h-4 w-4" />
               Compartilhar WhatsApp
