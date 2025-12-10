f3f46d2d417ef334d0daa27964b276f35233effa
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 23:24:36 +0000

    ISSO AI, SO QUE OS TRAÇOS PRECIS SER UM POUQUINHO MAIOR DO QUE ESTA AGOR

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -44,7 +44,7 @@ export default function AppSidebar() {
                 style={{
                   backgroundImage:
                     'linear-gradient(to bottom, hsl(var(--sidebar-foreground)) 50%, transparent 50%)',
-                  backgroundSize: '1px 8px',
+                  backgroundSize: '1px 12px',
                   backgroundRepeat: 'repeat-y',
                 }}
               ></div>
