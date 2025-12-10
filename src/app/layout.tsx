07157d9b5efda210a324fe8a77015c785435165e
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 11:33:11 +0000

    a pagina fica dando zoom quando entro pelo celular, quero que tire isso

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
