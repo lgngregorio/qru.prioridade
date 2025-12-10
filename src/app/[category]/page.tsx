3bd734a0bef70d295a670fbcbe160b18a322e7a4
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 12:06:41 +0000

    aumente mais um pouco (_for element <CardDescription>_)

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -484,7 +484,7 @@ export default function ReportPage() {
             <CardTitle className="text-3xl font-bold text-foreground font-headline tracking-wide uppercase">
               {title}
             </CardTitle>
-            <CardDescription className="text-base mt-1 text-muted-foreground normal-case">
+            <CardDescription className="text-lg mt-1 text-muted-foreground normal-case">
              {description}
             </CardDescription>
           </CardHeader>
@@ -496,5 +496,7 @@ export default function ReportPage() {
     </main>
   );
 }
+
+    
 
     
