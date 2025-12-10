2d31e81828ba53fb10feacb1494ece202c4cbc4c
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 21:37:19 +0000

    deixe o nome menu proximo do icone, bem pertinho do jeito que esta, esta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-4 pt-8 md:p-6">
-      <div className="absolute top-2 left-2 z-10 flex flex-col items-center">
-        <span className="text-xs font-bold uppercase text-white">Menu</span>
+      <div className="absolute top-4 left-4 z-10 flex flex-col items-center">
+        <span className="text-sm font-bold uppercase text-white -mb-1">Menu</span>
         <SidebarTrigger className="md:hidden h-12 w-12" />
       </div>
       <div className="w-full max-w-md">
