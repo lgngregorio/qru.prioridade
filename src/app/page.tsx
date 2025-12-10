455beae68f68ac8dae4bf3fb7e39b781a763d3de
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 21:35:39 +0000

    escreve menu em cima do icone do menu pra ficar mais visivel esse botão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-4 pt-8 md:p-6">
-      <div className="absolute top-2 left-2 z-10">
-        <SidebarTrigger className="md:hidden h-14 w-14" />
+      <div className="absolute top-2 left-2 z-10 flex flex-col items-center">
+        <span className="text-xs font-bold uppercase text-white">Menu</span>
+        <SidebarTrigger className="md:hidden h-12 w-12" />
       </div>
       <div className="w-full max-w-md">
         <header className="text-center w-full mb-6">
