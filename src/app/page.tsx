5640fb8f19be94a39d6012ba136ce7e55f6570f2
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 23:25:47 +0000

    AUMENTA MAIS OS TRAÇOS DA MINHA TRACEJADA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                     style={{
                       backgroundImage:
                         'linear-gradient(to bottom, hsl(var(--foreground)) 50%, transparent 50%)',
-                      backgroundSize: '1px 12px',
+                      backgroundSize: '1px 28px',
                       backgroundRepeat: 'repeat-y',
                     }}
                   ></div>
