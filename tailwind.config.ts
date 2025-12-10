d8b272ba393b8d2d3bd6063127c3f2e5211b6654
Lugrena Game <lgn.croches@gmail.com>
2025-12-10 23:29:59 +0000

    AUMENTA MAIS UM POUQUINHO A VELOCIDADE

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,7 +104,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'move-dashes': 'move-dashes 1.9s linear infinite',
+        'move-dashes': 'move-dashes 1.5s linear infinite',
       },
     },
   },
