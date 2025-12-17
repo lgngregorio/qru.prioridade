d760cc0579e66b7797f6af3e2877ad05a81d6f2b
Lugrena Game <lgn.croches@gmail.com>
2025-12-17 14:16:40 +0000

    APANAS NESSA ABA DE TO02 COLOCAR EM CAIXA ALTA E EM NEGRITO (_for elemen

--- src/components/forms/IncendioForm.tsx
+++ src/components/forms/IncendioForm.tsx
@@ -269,7 +269,7 @@ export default function IncendioForm({ categorySlug }: { categorySlug: string })
               />
               <label
                 htmlFor="show-vtr-apoio"
-                className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                className="text-base font-bold uppercase leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
               >
                 Houve VTR de Apoio?
               </label>
