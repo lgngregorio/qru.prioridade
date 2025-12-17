d0cff691832c8a61dedf064e4cc4c8ea48c041fb
Lugrena Game <lgn.croches@gmail.com>
2025-12-17 21:06:20 +0000

    APENAS NESSA ABA TO34 COLOCAR EM CAIXA ALTA E EM NEGRITO (_for element s

--- src/components/forms/TO34Form.tsx
+++ src/components/forms/TO34Form.tsx
@@ -252,9 +252,9 @@ export default function TO34Form({ categorySlug }: { categorySlug: string }) {
               />
               <label
                 htmlFor="show-vtr-apoio"
-                className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                className="text-base font-bold uppercase leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
               >
-                Houve VTR de Apoio?
+                HOUVE VTR DE APOIO?
               </label>
             </div>
             {showVtrApoio && (
