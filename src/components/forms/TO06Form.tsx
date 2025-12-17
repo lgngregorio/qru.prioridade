a7e4a35f3de321d916b642155eacb1ec4c229a54
Lugrena Game <lgn.croches@gmail.com>
2025-12-17 20:22:43 +0000

    APENAS NESSA ANA DE TO06 COLOCAR EM CAIXA ALTA E EM NEGRITO (_for elemen

--- src/components/forms/TO06Form.tsx
+++ src/components/forms/TO06Form.tsx
@@ -447,7 +447,7 @@ export default function TO06Form({ categorySlug }: { categorySlug: string }) {
               />
               <label
                 htmlFor="show-vtr-apoio"
-                className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                className="text-base font-bold uppercase leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
               >
                 Houve VTR de Apoio?
               </label>
