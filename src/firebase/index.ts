63b0264fe0d932ec97b4fa8e906e790fa84c206d
Lugrena Game <lgn.croches@gmail.com>
2025-12-18 01:09:02 +0000

    Try fixing this error: `Runtime Error: Firebase API Key is missing. Make

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
 
 let app: FirebaseApp | null = null;
 let auth: Auth | null = null;
@@ -10,6 +9,15 @@ let firestore: Firestore | null = null;
 
 export function getFirebaseInstances() {
     if (typeof window !== 'undefined' && !app) {
+        const firebaseConfig = {
+            projectId: "studio-2284671180-4b3bb",
+            appId: "1:1092911829966:web:dfcf6e3720a1a77bddd19f",
+            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+            authDomain: "studio-2284671180-4b3bb.firebaseapp.com",
+            measurementId: "",
+            messagingSenderId: "1092911829966"
+        };
+        
         if (!firebaseConfig.apiKey) {
             throw new Error("Firebase API Key is missing. Make sure NEXT_PUBLIC_FIREBASE_API_KEY is set in your .env.local file.");
         }
